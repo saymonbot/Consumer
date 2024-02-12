6f42b14451fab6d5ebd203b31e0221ebae768593
saymonbot <saymon_botelho123@outlook.com>
2024-02-12 16:12:26 -0300

    Declareted Lambda Expression

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 import entities.Product;
 
@@ -16,7 +17,11 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(Product::nonStaticPriceUpdate);
+		Consumer<Product> cons = p -> {
+			p.setPrice(p.getPrice() * 1.1);
+		};
+		
+		list.forEach(cons);
 		
 		list.forEach(System.out::println);
 	}
