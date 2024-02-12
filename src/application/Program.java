c67f96befd39e7df989978b3bc3b9460296b1524
saymonbot <saymon_botelho123@outlook.com>
2024-02-12 16:13:30 -0300

    Inline Lambda Expression

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,6 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
 
 import entities.Product;
 
@@ -17,11 +16,8 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		Consumer<Product> cons = p -> {
-			p.setPrice(p.getPrice() * 1.1);
-		};
 		
-		list.forEach(cons);
+		list.forEach(p -> p.setPrice(p.getPrice() * 1.1));
 		
 		list.forEach(System.out::println);
 	}
